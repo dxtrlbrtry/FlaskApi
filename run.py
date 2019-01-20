fccbdaa3ee3d3f064f17cfe9c222b75ba0fb9b59
dxtrlbrtry <dxtrlbrtry@yahoo.com>
2019-01-20 17:34:24 +0200

    working version of run.py

--- run.py
+++ run.py
@@ -7,9 +7,9 @@ db = SQLAlchemy()
 jwt = JWTManager()
 
 
-if __name__ == '__main__':
+def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Projects/FlaskApi/database.db'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Projects/FlaskAPI/database.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['SECRET_KEY'] = 'key'
 
@@ -18,8 +18,14 @@ if __name__ == '__main__':
 
     from Controllers.user_controller import user_blueprint
     from Controllers.data_controller import data_blueprint
-    from Controllers.user_data_relationship_controller import user_data_association_blueprint
+    from Controllers.user_data_relation_controller import user_data_association_blueprint
     app.register_blueprint(user_blueprint)
     app.register_blueprint(data_blueprint)
     app.register_blueprint(user_data_association_blueprint)
+
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True, port=5000)
